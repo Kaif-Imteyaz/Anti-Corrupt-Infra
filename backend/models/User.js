76007a9d4dcae7f6867a98adec382aebe97d2df9
Kaif Imteyaz <kaifimteyaz.k@gmail.com>
2025-04-06 21:21:41 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,11 +1,23 @@
+// const mongoose = require("mongoose");
+
+// const UserSchema = new mongoose.Schema({
+//   name: String,
+//   email: { type: String, unique: true },
+//   password: String,
+//   role: { type: String, enum: ["citizen", "ward_member", "gram_panchayat"], default: "citizen" },
+//   govCredential: String, // For officials
+// });
+
+// module.exports = mongoose.model("User", UserSchema);
+
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-  name: String,
-  email: { type: String, unique: true },
-  password: String,
-  role: { type: String, enum: ["citizen", "ward_member", "gram_panchayat"], default: "citizen" },
-  govCredential: String, // For officials
-});
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  role: { type: String, enum: ["citizen", "ward_member", "gram_panchayat"], default: "citizen", required: true },
+  govCredential: { type: String, required: true }, // For officials
+}, //{ timestamps: true });
 
 module.exports = mongoose.model("User", UserSchema);
